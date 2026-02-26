4cb31b631b9be827f9b316676381010afb84edb6
ThierryTorino <thierry.torino@yahoo.com>
2026-02-25 22:15:48 -0300

    sobre mim att

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -11,14 +11,14 @@ import { FaGithub } from "react-icons/fa";
 
 function Resume(){
     const tecnologias = [
-        {nome: "JavaScript", cor: "bg-yellow-500", icone: <IoLogoJavascript/>},
-        {nome: "React", cor: "bg-cyan-400", icone: <FaReact/>},
-        {nome: "Cpp", cor: "bg-blue-500", icone: <TbBrandCpp/>},
-        {nome: "Git", cor: "bg-orange-500", icone: <FaGitAlt/>},
-        {nome: "Tailwind", cor: "bg-cyan-500", icone: <RiTailwindCssFill/>},
-        {nome: "Html", cor: "bg-orange-500", icone: <FaHtml5/>},
-        {nome: "Css", cor: "bg-blue-500", icone: <FaCss3/>},
-        {nome: "GitHub", cor: "bg-zinc-700", icone: <FaGithub/>}
+        {nome: "JavaScript", cor: "text-yellow-500 hover:border-yellow-500", icone: <IoLogoJavascript/>},
+        {nome: "React", cor: "text-cyan-400 hover:border-cyan-400", icone: <FaReact/>},
+        {nome: "Cpp", cor: "text-blue-500 hover:border-blue-500", icone: <TbBrandCpp/>},
+        {nome: "Git", cor: "text-orange-500 hover:border-orange-500", icone: <FaGitAlt/>},
+        {nome: "Tailwind", cor: "text-cyan-500 hover:border-cyan-500", icone: <RiTailwindCssFill/>},
+        {nome: "Html", cor: "text-orange-500 hover:border-orange-500", icone: <FaHtml5/>},
+        {nome: "Css", cor: "text-blue-500 hover:border-blue-500", icone: <FaCss3/>},
+        {nome: "GitHub", cor: "text-zinc-100 hover:border-zinc-100", icone: <FaGithub/>}
     ]
 
     return(
@@ -29,17 +29,17 @@ function Resume(){
 
             <div className="flex flex-row justify-between">
                 <div className="max-w-2xl">
-                    <p className="text-zinc-200 text-xl leading-relaxed text-left p-8 md:p-10 bg-white/5 backdrop-blur-md border border-white/10 rounded-3xl shadow-2xl ml-20 ">Estudante de Ciências da Computação e desenvolvedor focado no ecossistema JavaScript. Buscando me aprofundar em tecnologias modernas como React.js e Next.js. Busco minha primeira experiência na área, para me desenvolver profissionalmente, aplicar minhas habilidades e me aprimorar cada vez mais.</p>
+                    <p className="text-zinc-200 text-xl leading-relaxed w-full text-left p-8 md:p-10 bg-white/5 backdrop-blur-md border border-white/10 rounded-3xl shadow-2xl ml-20 ">Estudante de Ciências da Computação e desenvolvedor focado no ecossistema <span className="text-yellow-500 font-bold">JavaScript</span>. Buscando me aprofundar em tecnologias modernas como React.js e Next.js. Busco minha primeira experiência na área, para me desenvolver profissionalmente, aplicar minhas habilidades e me aprimorar cada vez mais.</p>
 
-                    <button className="text-white p-8 md:p-10 ml-20 bg-violet-500 w-148 rounded-2xl gap-10 mt-5">VER PROJETOS</button>
+                    <button className="text-white px-8 py-4 ml-20 bg-violet-500 w-full rounded-2xl gap-10 mt-15 shadow-2xl shadow-violet-500">VER PROJETOS</button>
                 </div>
 
                 <div className="flex flex-col mr-20 justify-center items-center gap-5">
                     <h2 className="text-white font-changaOne text-5xl">Tecnologias</h2>
-                    <ul className="text-white grid grid-cols-2 gap-4">
+                    <ul className="text-white grid grid-cols-2 gap-4 mt-10">
                         {tecnologias.map((tech) =>{
                             return(
-                                <li className={`${tech.cor}`} key={tecnologias.nome}>{tech.icone}{tech.nome}</li>
+                                <li className={`flex items-center gap-3 bg-white/5 border border-white/10 px-5 py-3 rounded-full hover:bg-white/10 hover:${tech.cor} hover:scale-105 transition-all cursor-default shadow-lg ${tech.cor}`} key={tech.nome}>{tech.icone}{tech.nome}</li>
                             )
                         })}
                     </ul>
